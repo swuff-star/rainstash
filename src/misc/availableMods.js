a7d2b086ac895157ab8744bca7aff5176ae5ebbd
swuff-star <63352264+swuff-star@users.noreply.github.com>
2021-02-25 02:05:59 -0500

    added ss2 to list

--- src/misc/availableMods.js
+++ src/misc/availableMods.js
@@ -16,7 +16,7 @@ export default {
                 'description': <>
                     An expansion made by 
                     <span style={{color: "#F75B00"}}> NK </span> 
-                    (<span style={{color: "#F75B00"}}>NK#0309</span> on Discord) that adds new survivors, items, artifacts, stages, enemies and much more to Risk of Rain.
+                    (<span style={{color: "#F75B00"}}>NK#0309</span> on Discord) that adds new survivors, items, artifacts, stages, enemies, and much more to Risk of Rain.
                     Go pick it up over at
                     <a target='_blank' rel='noopener noreferrer' href='https://rainfusion.ml/'> Rainfusion</a>.
                 </>
@@ -35,7 +35,15 @@ export default {
         'items': {
             'vanilla': {
                 'description': 'Vanilla items. Comes with the base game.'
-            }   
+            },
+            'starstorm 2': {
+                     'description': <>
+                        An expansion made by
+                     <span style=([color: "#F75B00"}}> Team Moonstorm </span> (<span style={{color: "#F75B00"}}>https://discord.gg/WYWZEAZjac</span> on Discord) that adds new survivors, items, enemies, and much more to Risk of Rain 2.
+                     Go pick it up over at
+                     <a target='_blank' rel='noopener noreferrer' href='https://thunderstore.io/package/TeamMoonstorm/Starstorm2/'> Thunderstore</a>.
+                     </>
+            }
         }
     }
 };
